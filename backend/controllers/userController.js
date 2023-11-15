efab066f1a3e788ef15346cf8f4ae586568018bb
root <root@Unaislaptop>
2023-11-15 08:37:33 +0530

    just registered user and hashed the password before saving the data into database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,38 @@
 import asyncHandler from 'express-async-handler'
+import User from '../model/userModel.js';
 
 const authUser = asyncHandler(async(req,res)=>{
+    
     res.status(200).send({message : 'User has authorized'})
 });
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).send({message : 'User has created'})
+    const { name, email, password} = req.body;
+
+    const userExist = await User.findOne({email})
+
+    if(userExist){
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    const user = await User.create({
+        name : name,
+        email : email,
+        password : password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id : user._id,
+            name : user.name,
+            email : user.email
+        })
+    }else{
+        res.status(400);
+        throw new Error('Invalid user data')
+    }
+   
 });
 
 const logoutUser = asyncHandler(async(req,res)=>{
--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,25 @@ connectDB() //calling mongoDB connected function that is imported from config fo
 
 const app = express();
 
+// Middleware to parse incoming JSON data in the request body.
+// This allows us to access the JSON data in route handlers using req.body.
+app.use(express.json());
+
+// Middleware to parse incoming URL-encoded data in the request body.
+// The extended option set to true allows handling rich objects and arrays in the URL-encoded data.
+// This middleware makes the parsed data available in route handlers using req.body.
+app.use(express.urlencoded({extended:true}))
+
 app.use('/api/users',userRoutes)
 
 app.get('/',(req,res)=>{
     res.send('Server is ready')
 })
 
+// Handle cases when no route matches the request with the 'notFound' middleware
 app.use(notFound);
 
+// Handle errors and provide consistent responses with the 'errorHandler' middleware
 app.use(errorHandler)
 
 app.listen(port,()=>{

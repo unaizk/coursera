74e50286765d65ee48d690ecf98da27d0537d745
root <root@Unaislaptop>
2023-11-16 21:14:13 +0530

    configured the navbar if there is user it will show different navbar by checking userInfo

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import {Navbar , Nav, Container} from 'react-bootstrap'
+import {Navbar , Nav, NavDropdown, Badge} from 'react-bootstrap'
 import {FaSignInAlt, FaSignOutAlt} from 'react-icons/fa'
 import {LinkContainer} from 'react-router-bootstrap'
+import { useDispatch ,useSelector } from 'react-redux'
+
 
 const Header = () => {
+
+    const {userInfo} = useSelector((state) => state.auth)
   return (
     <header>
     <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <>
-           <LinkContainer to='/' style={{marginLeft: '50px', fontSize: "24px" }}>
+           <LinkContainer to='/' style={{marginLeft: '100px', fontSize: "24px" }}>
            <Navbar.Brand >COURSERA</Navbar.Brand>
            </LinkContainer>
             
@@ -16,18 +20,39 @@ const Header = () => {
             <Navbar.Toggle aria-controls='basic-navbar-nav'/>
             <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className='ms-auto'>
-                   
-                <LinkContainer to='/login' style={{marginRight : "50px"}}>
-                        <Nav.Link >
-                            <FaSignInAlt/>Sign In
-                        </Nav.Link>
-                </LinkContainer>
-                
-                <LinkContainer to='/register' style={{marginRight : "50px"}}>
-                        <Nav.Link>
-                            <FaSignOutAlt/>Sign Up
-                        </Nav.Link>
-                </LinkContainer>
+                   {userInfo ? (
+                    <>
+                    <NavDropdown title={userInfo.name} id='username' style={{marginRight : '100px', fontSize: "20px", fontWeight:'bold'}}>
+                        <LinkContainer to={'/dashboard'}>
+                            <NavDropdown.Item>
+                                Dashboard
+                            </NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to={'/logout'}>
+                            <NavDropdown.Item>
+                                Logout
+                            </NavDropdown.Item>
+                        </LinkContainer>
+                        
+                    </NavDropdown>
+                    </>
+                   ) : (
+                    <>
+                    <LinkContainer to='/login' style={{marginRight : "30px"}}>
+                    <Nav.Link >
+                        <FaSignInAlt/>Sign In
+                    </Nav.Link>
+                   </LinkContainer>
+            
+                   <LinkContainer to='/register' style={{marginRight : "100px"}}>
+                    <Nav.Link>
+                        <FaSignOutAlt/>Sign Up
+                    </Nav.Link>
+                   </LinkContainer>
+                    </>
+                  
+                   )}
+               
                 </Nav>
             </Navbar.Collapse>
         </>
